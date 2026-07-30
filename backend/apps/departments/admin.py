b736f76e240b41a2d7bbca441d0fda021d385ad8
Abid Hasan <likhon2422@gmail.com>
2026-07-30 21:41:14 +0600

    add in register admin

--- backend/apps/departments/admin.py
+++ backend/apps/departments/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Department
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
 
-# Register your models here.
+    list_display = (
+        "id",
+        "name",
+        "code",
+        "is_active",
+        "created_at",
+    )
+
+    search_fields = (
+        "name",
+        "code",
+    )
+
+    list_filter = (
+        "is_active",
+        "created_at",
+    )
+
+    ordering = (
+        "name",
+    )
+
+    list_per_page = 20
