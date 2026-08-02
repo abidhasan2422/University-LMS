e7c16f890cc0fed659409f2eb006ccdadf6cc4cb
Abid Hasan <likhon2422@gmail.com>
2026-08-02 22:31:12 +0600

    add semester model validation

--- backend/apps/semester/models.py
+++ backend/apps/semester/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-
+from django.core.exceptions import ValidationError
 from apps.common.models import BaseModel
-
+from datetime import date
+from django.core.validators import MinValueValidator
 
 class Semester(BaseModel):
     """
@@ -18,12 +19,29 @@ class Semester(BaseModel):
         choices=SemesterName.choices,
     )
 
-    year = models.PositiveIntegerField()
+    year = models.PositiveIntegerField(
+            validators=[MinValueValidator(2000)]
+    )
 
     start_date = models.DateField()
 
     end_date = models.DateField()
+    def clean(self):
+        super().clean()
+        if self.start_date >= self.end_date:
+            raise ValidationError({
+                "end_date": "End date must be after start date."
+            })
+        current_year = date.today().year
 
+        if self.year > current_year + 5:
+            raise ValidationError({
+                "year": f"Year cannot be greater than {current_year + 5}."
+            })
+        
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
     class Meta:
         db_table = "semesters"
         ordering = ["-year", "name"]
@@ -34,7 +52,11 @@ class Semester(BaseModel):
             models.UniqueConstraint(
                 fields=["name", "year"],
                 name="unique_semester_per_year",
-            )
+            ),
+            models.CheckConstraint(
+            check=models.Q(start_date__lt=models.F("end_date")),
+            name="semester_start_before_end",
+    ),
         ]
 
     def __str__(self):
